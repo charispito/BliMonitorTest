652c6fd968284d30bbb2a5a70896658771f28e0f
Administrator <Administrator@JANG>
2026-06-29 16:00:27 +0900

    26.06.29 불필요한 소스 제거

--- BliMonitorTest/server/ClientData.cs
+++ BliMonitorTest/server/ClientData.cs
@@ -10,7 +10,6 @@ namespace BliMonitorTest.util
 {
     public class ClientData
     {
-        public ChannelItem channel { get; set; }
         public int ResponseCount { get; set; }
         public bool Start { get; set; } = false;
         public DateTime StartTime { get; set; }
@@ -42,25 +41,6 @@ namespace BliMonitorTest.util
             readCompleteData = new List<byte>();
             readParameterData = new List<byte>();
             StartTime = new DateTime();
-        }
-
-        // ✅ 더미용 생성자 추가
-        public ClientData(BliMonitorTest.controls.ChannelItem channel, int clientNumber)
-        {
-            this.channel = channel;
-            this.clientNumber = clientNumber;
-
-            // TCP 없음
-            this.client = null;
-
-            // 기존 로직과 동일하게 "유니크 키" 역할
-            TimeMills = DateTimeOffset.Now.Ticks + clientNumber;
-
-            // 더미 루프 제어
-            Run = true;
-
-            // 초기 카운트 (기존 코드 흐름에 맞춰)
-            initCount = 0;
         }
     }
 }
